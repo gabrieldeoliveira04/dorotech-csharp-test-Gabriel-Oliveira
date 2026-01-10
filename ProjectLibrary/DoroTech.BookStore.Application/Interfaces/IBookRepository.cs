652b6acacfd174f24cfcae3305208b95961fd44d
Gabriel Oliveira <77246516+gabrieldeoliveira04@users.noreply.github.com>
2026-01-10 16:07:12 +0000

    Feature: criação da interface book, com paginação e filtro implementados

---
+++ ProjectLibrary/DoroTech.BookStore.Application/Interfaces/IBookRepository.cs
@@ -0,0 +1,14 @@
+using DoroTech.BookStore.Domain.Entities;
+
+namespace DoroTech.BookStore.Application.Interfaces
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetAllAsync(int page, int pageSize, string? title);
+        Task<Book?> GetByIdAsync(Guid id);
+        Task<Book?> GetByTitleAsync(string title);
+        Task AddAsync(Book book);
+        Task UpdateAsync(Book book);
+        Task DeleteAsync(Book book);
+    }
+}
