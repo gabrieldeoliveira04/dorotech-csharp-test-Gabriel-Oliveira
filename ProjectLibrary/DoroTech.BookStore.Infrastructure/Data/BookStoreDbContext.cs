9116416b7121a0178851ccc08c18709cd7d8b3d5
Gabriel Oliveira <77246516+gabrieldeoliveira04@users.noreply.github.com>
2026-01-10 16:04:13 +0000

    Feature: criação do dbbcontext e adição da entidade book ao mesmo com validação em caso de livro ja cadastrado

---
+++ ProjectLibrary/DoroTech.BookStore.Infrastructure/Data/BookStoreDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using DoroTech.BookStore.Domain.Entities;
+
+namespace DoroTech.BookStore.Infrastructure.Data
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+            : base(options) { }
+
+        public DbSet<Book> Books => Set<Book>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasIndex(b => b.Title)
+                .IsUnique();
+        }
+    }
+}
