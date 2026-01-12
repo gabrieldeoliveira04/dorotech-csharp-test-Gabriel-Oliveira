c8520bcd8d53ccf809795130bd3f9bca79f56cad
Gabriel Oliveira <77246516+gabrieldeoliveira04@users.noreply.github.com>
2026-01-12 19:00:54 +0000

    Fix: Refinamento do bookrequest para melhor compreensão nos schemas

--- ProjectLibrary/DoroTech.BookStore.Application/DTOs/BookRequest.cs
+++ ProjectLibrary/DoroTech.BookStore.Application/DTOs/BookRequest.cs
@@ -1,9 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DoroTech.BookStore.Application.DTOs
 {
-    public record BookRequest(
-        string Title,
-        string Author,
-        decimal Price,
-        int Stock
-    );
+    public class BookRequest
+    {
+        [Required]
+        [StringLength(150, MinimumLength = 2)]
+        public string Title { get; set; } = default!;
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Author { get; set; } = default!;
+
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Stock { get; set; }
+    }
 }
+
